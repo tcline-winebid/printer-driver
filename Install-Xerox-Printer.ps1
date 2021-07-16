d5ca58304584e977717b1b63a7d40a46a55e60ca
tcline-winebid <87541665+tcline-winebid@users.noreply.github.com>
2021-07-16 14:18:46 -0700

    Update Install-Xerox-Printer.ps1

--- Install-Xerox-Printer.ps1
+++ Install-Xerox-Printer.ps1
@@ -1,12 +1,12 @@
-﻿ $repo = 'https://github.com/tcline-winebid/printer-driver/archive/8109db7ece3643f08ae7c454fa422ec3fc999905.zip'
- $repoOut = 'C:\Windows\LTSvc\VersaLink_B600_B605_B610_B615_7.95.0.0_PCL6_x64.zip'
  $outZip = 'C:\Windows\LTSvc\XeroxB610Drivers\'
  $finalUnzip = 'C:\Windows\LTSvc\FinalZip_Xerox\'
  $PrintDriver = "Xerox VersaLink B600 PCL6"
  $printPort = "192.168.40.168"
  $printName = "Xerox VersaLink B600"
+ $repo = 'https://github.com/tcline-winebid/printer-driver/archive/8109db7ece3643f08ae7c454fa422ec3fc999905.zip'
+ $repoOut = 'C:\Windows\LTSvc\VersaLink_B600_B605_B610_B615_7.95.0.0_PCL6_x64.zip'
  
- Invoke-RestMethod -Uri $repo -OutFile $repoOut
+
 
  Expand-Archive -Path $repoOut -DestinationPath $outZip
 
